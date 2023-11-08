2eb2ec5fbe7ed4368d6702b0ac1798c760e29505
AJ Abdujabborov <60819490+aj-abdujabborov@users.noreply.github.com>
2023-11-08 20:38:21 +0500

    Refactor to use classes

--- script.js
+++ script.js
@@ -1,72 +1,81 @@
-const myLibrary = [];
-const form = {
-    form: document.querySelector("form#add-book"),
-    title: document.querySelector("form#add-book input#title"),
-    author: document.querySelector("form#add-book input#author"),
-    year: document.querySelector("form#add-book input#year"),
-    read: document.querySelector("form#add-book input#read"),
-    button: document.querySelector("form#add-book button"),
-};
-const bookContainer = document.querySelector("div.book-container");
-const bookTemplate = document.querySelector("template#book-template");
-const addBookCard = document.querySelector("div.add-book");
+class DOM {
+    static bookContainer = document.querySelector("div.book-container");
+    static bookTemplate = document.querySelector("template#book-template");
+    static addBookCard = document.querySelector("div.add-book");
 
-form.button.addEventListener("click", () => {
-    addBookToLibrary(form.title.value,
-                    form.author.value,
-                    form.year.value,
-                    form.read.checked);
-    form.form.reset();
-})
-
-addBookToLibrary("Philosopher's Stone", "J. K. Rowling", 1997, true);
-addBookToLibrary("Chamber of Secrets", "J. K. Rowling", 1998, true);
-addBookToLibrary("Prisoner of Azkaban", "J. K. Rowling", 1999, false);
-addBookToLibrary("Goblet of Fire", "J. K. Rowling", 2000, false);
-
-function addBookToLibrary(title, author, year, bRead) {
-    const newBook = new Book(title, author, year, bRead);
-    myLibrary.push(newBook);
+    static addBookForm = {
+        form: document.querySelector("form#add-book"),
+        title: document.querySelector("form#add-book input#title"),
+        author: document.querySelector("form#add-book input#author"),
+        year: document.querySelector("form#add-book input#year"),
+        read: document.querySelector("form#add-book input#read"),
+        button: document.querySelector("form#add-book button"),
+    }
 }
 
-function Book(title, author, year, bRead) {
-    if (!(this instanceof Book)) {
-        throw Error('Error: should be invoked as object constructor');
-    }
-    setInternalValues.call(this);
+class Book {
+    title = "Unknown";
+    author = "Unknown";
+    year = "Unknown";
+    #bRead = false;
 
-    const clone = bookTemplate.content.cloneNode(true);
-    populateCloneDetails.call(this, clone);
-    const bookInstance = insertCloneIntoDOM(clone);
-    bookInstance.querySelector("img.minus-icon").addEventListener("click", () => {
-        bookInstance.remove();
-        
-        const ind = myLibrary.indexOf(this);
-        myLibrary.splice(ind, 1);
-    });
-    const readStatus = bookInstance.querySelector("span#read.value");
-    readStatus.addEventListener("click", () => {
-        this.read = !this.read;
-        readStatus.textContent = this.read ? "Read ✔" : "Not read ✘";
+    constructor(title, author, year, read) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.#bRead = read;
+    }
+
+    invertReadState() {
+        this.#bRead = !this.#bRead;
+    }
+
+    getReadString() {
+        return this.#bRead ? "Read ✔" : "Not read ✘";
+    }
+}
+
+(function Launch() {
+    const myLibrary = [];
+
+    const addBookToLibrary = (title, author, year, read) => {
+        const newBook = new Book(title, author, year, read);
+        myLibrary.push(newBook);
+
+        const filledTemplate = getFilledTemplate();
+        DOM.bookContainer.insertBefore(filledTemplate, DOM.addBookCard);
+        const bookInDOM = DOM.addBookCard.previousElementSibling;
+
+        bookInDOM.querySelector("span#read.value").addEventListener("click", (event) => {
+            newBook.invertReadState();
+            event.target.textContent = newBook.getReadString();
+        })
+
+        bookInDOM.querySelector("img.minus-icon").addEventListener("click", () => {
+            bookInDOM.remove();
+            myLibrary.splice(myLibrary.indexOf(newBook), 1);
+        });
+
+        function getFilledTemplate() {
+            const template = DOM.bookTemplate.content.cloneNode(true);
+            template.querySelector("span#title.value").textContent = newBook.title;
+            template.querySelector("span#author.value").textContent = newBook.author;
+            template.querySelector("span#year.value").textContent = newBook.year;
+            template.querySelector("span#read.value").textContent = newBook.getReadString();
+            return template;
+        }
+    }
+
+    DOM.addBookForm.button.addEventListener("click", () => {
+        addBookToLibrary(DOM.addBookForm.title.value,
+                         DOM.addBookForm.author.value,
+                         DOM.addBookForm.year.value,
+                         DOM.addBookForm.read.checekd);
+        DOM.addBookForm.form.reset();
     })
 
-    function setInternalValues() {
-        this.title = title ? title : "Unknown";
-        this.author = author ? author : "Unknown";
-        this.year = year ? Number(year) : "Unknown";
-        this.read = bRead ? true : false;
-    }
-
-    function populateCloneDetails(clone) {
-        clone.querySelector("span#title.value").textContent = this.title;
-        clone.querySelector("span#author.value").textContent = this.author;
-        clone.querySelector("span#year.value").textContent = this.year;
-        clone.querySelector("span#read.value").textContent = this.read ? "Read ✔" : "Not read ✘"
-    }
-
-    function insertCloneIntoDOM(clone) {
-        bookContainer.insertBefore(clone, addBookCard);
-        return addBookCard.previousElementSibling;
-    }
-}
-
+    addBookToLibrary("Philosopher's Stone", "J. K. Rowling", 1997, true);
+    addBookToLibrary("Chamber of Secrets", "J. K. Rowling", 1998, true);
+    addBookToLibrary("Prisoner of Azkaban", "J. K. Rowling", 1999, false);
+    addBookToLibrary("Goblet of Fire", "J. K. Rowling", 2000, false);
+})();
